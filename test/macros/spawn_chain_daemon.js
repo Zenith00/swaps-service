92823e91700689e20912a162c4dd97b44ef82f27
Zenith00 <austin@blockseer.com>
2018-07-02 08:54:15 -0700

    Fix datadir missing error

--- test/macros/spawn_chain_daemon.js
+++ test/macros/spawn_chain_daemon.js
@@ -76,7 +76,7 @@ module.exports = (args, cbk) => {
       fs.mkdirSync(tmpDir);
 
       daemon = spawn(executable, [
-        `-datadir='${tmpDir}'`,
+        '-datadir', tmpDir,
         '-debuglogfile', tmpDir,
         // '--miningaddr', fromPublicKeyBuffer(miningKey, network).getAddress(),
         // '--notls',
