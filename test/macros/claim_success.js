bcf3ff0cc17adc6aeeff150471c3b584d4246496
Zenith00 <austin@blockseer.com>
2018-07-02 12:02:17 -0700

    Debug info

--- test/macros/claim_success.js
+++ test/macros/claim_success.js
@@ -9,7 +9,7 @@ const {findSwapTransaction} = require('./../../service');
 const {generateChainBlocks} = require('./../../chain');
 const generateInvoice = require('./generate_invoice');
 const {generateKeyPair} = require('./../../chain');
-const {getCurrentHeight} = require('./../../chain'); 
+const {getCurrentHeight} = require('./../../chain');
 const mineTransaction = require('./mine_transaction');
 const sendChainTokensTransaction = require('./send_chain_tokens_tx');
 const spawnChainDaemon = require('./spawn_chain_daemon');
@@ -116,7 +116,7 @@ module.exports = (args, cbk) => {
 
     // Bob needs to go get a block to spend his block reward to the swap
     bobUtxo: ['generateToMaturity', ({generateToMaturity}, cbk) => {
-      debugger;
+      console.log("generateToMaturity begin")
       const [firstRewardBlock] = generateToMaturity.blocks;
 
       const [coinbaseTransaction] = firstRewardBlock.transactions;
@@ -133,7 +133,7 @@ module.exports = (args, cbk) => {
 
     // Bob makes a send transaction to fund the swap with his coins
     fundSwapAddress: ['bobUtxo', 'createChainSwapAddress', (res, cbk) => {
-      debugger;
+      console.log("sendChainTokensTransactionStart");
       return sendChainTokensTransaction({
         destination: res.createChainSwapAddress[`${args.swap_type}_address`],
         network: args.network,
@@ -147,7 +147,7 @@ module.exports = (args, cbk) => {
 
     // The chain progresses and confirms the swap funding
     mineFundingTx: ['fundSwapAddress', ({fundSwapAddress}, cbk) => {
-      debugger;
+      console.log("mineFundingTx");
       return mineTransaction({
         network: args.network,
         transaction: fundSwapAddress.transaction,
@@ -164,7 +164,7 @@ module.exports = (args, cbk) => {
       'mineFundingTx',
       (res, cbk) =>
     {
-      debugger;
+      console.log("findFundingTransaction start");
       const isPkHash = !!args.is_refund_to_public_key_hash;
 
       const refundPkHash = !isPkHash ? null : res.generateBobKeyPair.pk_hash;
@@ -195,7 +195,7 @@ module.exports = (args, cbk) => {
       'findFundingTransaction',
       ({createChainSwapAddress, findFundingTransaction}, cbk) =>
     {
-      debugger;
+      console.log("fundingTransactionUtxos start");
       if (!findFundingTransaction.transaction) {
         return cbk([0, 'ExpectedFundedSwapTransaction']);
       }
@@ -231,7 +231,7 @@ module.exports = (args, cbk) => {
     // Test `claim_fail_preimage` where the claim is attempted with bad preimg
     claimWithBadPreimage: ['readyToClaim', ({readyToClaim}, cbk) => {
       try {
-        debugger;
+        console.log("claimWithBadPreimage start");
         return cbk(null, claimTransaction({
           current_block_height: readyToClaim.current_block_height,
           destination: readyToClaim.destination,
@@ -248,7 +248,7 @@ module.exports = (args, cbk) => {
 
     // Make sure that using a bad preimage fails the claim tx broadcast
     confirmFailWithBadPreimage: ['claimWithBadPreimage', (res, cbk) => {
-      debugger;
+      console.log('confirmFailWithBadPreimage start');
       return broadcastTransaction({
         network: args.network,
         transaction: res.claimWithBadPreimage.transaction,
@@ -264,7 +264,7 @@ module.exports = (args, cbk) => {
 
     // Test `claim_fail_sig` where the claim is attempted with a bad sig
     claimWithBobSig: ['generateBobKeyPair', 'readyToClaim', (res, cbk) => {
-      debugger;
+      console.log("claimWithBobSig start");
       try {
         return cbk(null, claimTransaction({
           current_block_height: res.readyToClaim.current_block_height,
@@ -282,7 +282,7 @@ module.exports = (args, cbk) => {
 
     // Make sure that using a bad claim signature fails the tx broadcast
     confirmFailWithBadSig: ['claimWithBobSig', ({claimWithBobSig}, cbk) => {
-      debugger;
+      console.log("confirmFailWithBadSig start");
       return broadcastTransaction({
         network: args.network,
         transaction: claimWithBobSig.transaction,
@@ -308,7 +308,7 @@ module.exports = (args, cbk) => {
 
     // Alice paid Bob's invoice so she now uses that preimage for the reward
     claimTransaction: ['readyToClaim', ({readyToClaim}, cbk) => {
-      debugger;
+      console.log("claimTransaction start");
       try {
         return cbk(null, claimTransaction({
           current_block_height: readyToClaim.current_block_height,
@@ -326,6 +326,7 @@ module.exports = (args, cbk) => {
 
     // Alice's rewarded coins are confirmed back to an address she controls
     mineClaimTransaction: ['claimTransaction', ({claimTransaction}, cbk) => {
+      console.log("mineClaimTransaction");
       const {transaction} = claimTransaction;
 
       return mineTransaction({network: args.network, transaction}, cbk);
@@ -333,6 +334,7 @@ module.exports = (args, cbk) => {
   },
   (err, res) => {
     if (!!res.spawnChainDaemon && !!res.spawnChainDaemon.is_ready) {
+
       return stopChainDaemon({network: args.network}, stopErr => {
         return cbk(stopErr || err);
       });
