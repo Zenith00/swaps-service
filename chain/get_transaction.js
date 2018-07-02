ed1e72c20210352d5c7964c11bade2e77bf13eeb
Zenith00 <austin@blockseer.com>
2018-07-02 15:10:06 -0700

    sin

--- chain/get_transaction.js
+++ chain/get_transaction.js
@@ -3,16 +3,16 @@ const {getRawTransaction} = require('./conf/rpc_commands');
 
 /** Get a raw transaction
 
-  {
-    id: <Transaction Id String>
-    network: <Network Name String>
-  }
+ {
+   id: <Transaction Id String>
+   network: <Network Name String>
+ }
 
-  @returns via cbk
-  {
-    [transaction]: <Transaction Hex String>
-  }
-*/
+ @returns via cbk
+ {
+   [transaction]: <Transaction Hex String>
+ }
+ */
 module.exports = ({id, network}, cbk) => {
   if (!id) {
     return cbk([500, 'ExpectedIdForTransaction']);
@@ -23,40 +23,38 @@ module.exports = ({id, network}, cbk) => {
   }
   console.log("getting trans with: " + id);
   return chainRpc({
-    network,
-    cmd: getRawTransaction,
-    params: [id],
-  },
-  (err, transaction) => {
-    console.log("Ran gettrans");
-    if (!!err) {
-      return cbk(err);
-    }
-
-    if (!transaction){
-      console.log("init gettrans failed");
-      var start = new Date().getTime();
-      var end = start;
-      while(end < start + 10000) {
-        end = new Date().getTime();
+      network,
+      cmd: getRawTransaction,
+      params: [id],
+    },
+    (err, transaction) => {
+      console.log("Ran gettrans");
+      if (!!err) {
+        return cbk(err);
       }
+
+      if (!transaction) {
+        console.log("init gettrans failed");
+        var start = new Date().getTime();
+        var end = start;
+        while (end < start + 10000) {
+          end = new Date().getTime();
+        }
 
       return chainRpc({
           network,
-          cmd: "decoderawtransaction",
+          cmd: getRawTransaction,
           params: [id],
         },
         (err, transaction) => {
-        console.log("Backup: "  + transaction);
+          console.log("Ran gettrans");
           if (!!err) {
             return cbk(err);
           }
-
           return cbk(null, {transaction});
-        });
-    }
-
-    return cbk(null, {transaction});
-  });
+        })
+        }
+      return cbk(null, {transaction});
+    });
 };
 
