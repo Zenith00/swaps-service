ff7cba709082f6bd832f442a11fac123e82441ef
Zenith00 <austin@blockseer.com>
2018-07-03 10:20:56 -0700

    Cleanup

--- test/macros/spawn_chain_daemon.js
+++ test/macros/spawn_chain_daemon.js
@@ -98,7 +98,7 @@ module.exports = (args, cbk) => {
         '-txindex=1',
         '-printtoconsole=1',
         // '-debuglevel=info',
-        '-debug=rpc',
+        '-debug=1',
         // '-server',
         '-minrelaytxfee=0'
       ]);
