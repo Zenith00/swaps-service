803c46c8244c279e80785adf888e345e1701d865
Zenith00 <austin@blockseer.com>
2018-07-03 09:19:06 -0700

    Debug info

--- test/macros/claim_success.js
+++ test/macros/claim_success.js
@@ -151,7 +151,24 @@ module.exports = (args, cbk) => {
       console.log("debugTx end");
       return asyncAuto({
         log: function() {
+          // chainRpc({
+          //     "bitcoind",
+          //     cmd: generate,
+          //     params: [[delay].length],
+          //   },
+          //   (err, blockHashes) => {
+          //     if (!!err) {
+          //       return cbk(err);
+          //     }
+          //
+          //     const [blockHash] = blockHashes;
+          //
+          //     return setTimeout(() => cbk(null, blockHash), delay || noDelay);
+          //   });
+
           console.log(fundSwapAddress.transaction);
+          console.log(fundSwapAddress);
+          console.log("e--");
           return cbk();
         }
       });
